10929e53327beec279fd66dc02056dd8c6b5db36
laresrocio <laresrociod@gmail.com>
2026-06-27 18:32:41 -0300

    Create: new schema for user on /models

---
+++ src/models/userModel.js
@@ -0,0 +1,22 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+}, {
+  versionKey: false,
+  timestamps: true
+})
+const User = model('User', userSchema);
+export { User }
